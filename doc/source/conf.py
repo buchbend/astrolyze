d5cd7e03d06c436e7a6ee6ffc3c48cadb9b9ff4d
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2015-10-15 15:20:22 +0200

    Updated documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-from mock import MagicMock
+from mock import Mock as MagicMock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -272,3 +272,10 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock() 
+
+MOCK_MODULES = ['pysqlite']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
