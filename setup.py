6cb8fd4d7e8857f3bf1b74ec1d6958435148eacc
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2015-10-19 15:34:13 +0200

    Test workaround for read the docs

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ CONFIG_FOLDER = "/home/{}/.astrolyze/".format(SUDO_USER)
 data_files = [("/home/{}/.astrolyze/cfg/".format(SUDO_USER),
               ["cfg/calibration_error.txt", "cfg/galaxy_parameter.txt",
                "cfg/line_parameter.txt"])]
-if SUDO_USER == None:
-    data_file=[]
+if not SUDO_USER:
+    data_files=[]
 
 setup(
     name='astrolyze',
