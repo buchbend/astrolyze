fd4ad3987abd69e5f95dcd4b80882024e87eac76
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2015-10-20 14:23:47 +0200

    RTD fix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -279,5 +279,5 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['pysqlite2', 'pygreg', 'pyclass', 'pysic',
-                'generaltools', 'astropy']
+                'generaltools', 'astropy', "astropy.io"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
