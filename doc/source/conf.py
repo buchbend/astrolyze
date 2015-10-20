1a7649c1321efa3c81cb86834ea28c87983480ab
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2015-10-20 13:39:21 +0200

    added astropy to docu mocks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -278,5 +278,6 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pysqlite2', 'pygreg', 'pyclass', 'pysic', 'generaltools']
+MOCK_MODULES = ['pysqlite2', 'pygreg', 'pyclass', 'pysic',
+                'generaltools', 'astropy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
